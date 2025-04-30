c2cda8bf2c4036c4786b778a401e4aaa5e7e2226
0yu-uy0 <e1c24059@oit.ac.jp>
2025-04-30 15:12:51 +0900

    2025/04/30 15:12 中井 修正:ピンの重心

--- Assets/Script/General/PinBase.cs
+++ Assets/Script/General/PinBase.cs
@@ -5,11 +5,19 @@ public class PinBase : MonoBehaviour
 {
     [SerializeField] private float knockDownAngleThreshold = 45f;
 
+    public Vector3 center = new Vector3(0f, -0.6f, 0f);
+    private Rigidbody rb;
+
     private Quaternion initialRotation;
 
     private void Start()
     {
         initialRotation = transform.rotation;
+        rb = GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+            rb.centerOfMass = center;
+        }
     }
 
     public bool IsKnockedDownPin()
