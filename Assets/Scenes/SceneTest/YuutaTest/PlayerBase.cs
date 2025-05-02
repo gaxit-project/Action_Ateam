f0596a65322f9745841cb7d3416d750bc4a4509f
e1b24016 <e1b24016@oit.ac.jp>
2025-05-02 16:16:43 +0900

    2025/5/2 16:16 江端 Fix:PlayerBase

--- Assets/Scenes/SceneTest/YuutaTest/PlayerBase.cs
+++ Assets/Scenes/SceneTest/YuutaTest/PlayerBase.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBase : SingletonMonoBehaviour<PlayerBase>
@@ -12,9 +10,15 @@ public class PlayerBase : SingletonMonoBehaviour<PlayerBase>
     [SerializeField] protected float speed = 5f;
     [SerializeField] protected float weight = 10f;
     protected float rotation = 0f;
+    private Vector3 currentVelocity  = Vector3.zero;
 
     //移動関係
+    protected Vector3 x = Vector3.zero;
+    protected Vector3 z = Vector3.zero;
+    [SerializeField] protected float rayDistance = 1.2f;
     [SerializeField] protected float rotateSpeed = 100f;
+    [SerializeField] protected float acceleration = 5f;
+    [SerializeField] protected float deceleration = 5f;
     [SerializeField] private Vector3 gravity = new Vector3(0f, -20f, 0f);
 
 
@@ -40,7 +44,7 @@ public class PlayerBase : SingletonMonoBehaviour<PlayerBase>
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         //ステータスを取得
         speed = player.Speed;
@@ -51,16 +55,31 @@ public class PlayerBase : SingletonMonoBehaviour<PlayerBase>
         //transform.rotation = Quaternion.Euler(0f, rotation, 0f);
         if (rigidbody)
         {
-            //スティックまたはWASD,矢印キーで移動
-            Vector3 x = transform.right * Input.GetAxis("Horizontal") * speed;
-            Vector3 z = transform.forward * Input.GetAxis("Vertical") * speed;
-            rigidbody.MovePosition(rigidbody.position + (x + z) * Time.deltaTime); 
+            //地面についている場合のみスティックまたはWASD,矢印キーの入力を受付
+            if (player.IsGrounded(transform.position, rayDistance))
+            {
+                x = transform.right * Input.GetAxis("Horizontal") * speed;
+                z = transform.forward * Input.GetAxis("Vertical") * speed;
+            }
+
+            //ローカル座標に変換
+            Vector3 targetVelocity = transform.TransformDirection(Vector3.ClampMagnitude(x + z, 1f)) * speed;
+
+            //加減速処理
+            float lerpRate = (targetVelocity.magnitude > 0.1f) ? acceleration : deceleration;
+            currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, lerpRate * Time.fixedDeltaTime);
+
+            //移動
+            rigidbody.MovePosition(rigidbody.position + currentVelocity * Time.fixedDeltaTime);
+
+             //rigidbody.MovePosition(rigidbody.position + (x + z) * Time.deltaTime);
+                
             //rigidbody.linerVelocity = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")).normalized * speed * Time.deltaTime;
             //固有の重力
             rigidbody.AddForce(gravity, ForceMode.Acceleration);
         }
         
-        float mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime; //左右回転
+        float mouseX = Input.GetAxis("Mouse X") * rotateSpeed * Time.fixedDeltaTime; //左右回転
         rotation += mouseX;
         transform.Rotate(0f,rotation,0f);
             
@@ -82,6 +101,16 @@ public class PlayerBase : SingletonMonoBehaviour<PlayerBase>
         {
             Speed = speed;
             Weight = weight;
+        }
+
+        /// <summary>
+        /// 地面に接しているか判定する
+        /// </summary>
+        public bool IsGrounded(Vector3 pos, float distance)
+        {
+            Ray ray = new Ray(pos + Vector3.up * 0.1f, Vector3.down);
+
+            return true;//Physics.Raycast(ray, distance);
         }
 
     }
