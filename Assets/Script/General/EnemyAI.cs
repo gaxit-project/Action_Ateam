bfd771d3c0ed3284c9b7bb65799b30aeadb3999b
0yu-uy0 <e1c24059@oit.ac.jp>
2025-05-18 23:31:03 +0900

    2025/05/18 23:30 中井 add:ThrowingArea, ThrowingAreaManager

---
+++ Assets/Script/General/EnemyAI.cs
@@ -0,0 +1,27 @@
+using JetBrains.Annotations;
+using System.Security.Cryptography;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float weight = 10f;
+    [SerializeField] private float initialRotation = 0f;
+    [SerializeField] private float rayDistance = 2f;
+
+    private float rotation = 0f;
+    private Transform attackTarget;
+    private Vector3 currentVelocity = Vector3.zero;
+    private Animator attackAnimator;
+
+
+
+    public void Attacking()
+    {
+        attackAnimator.SetBool("Attack", true);
+    }
+
+}
---
+++ Assets/Script/General/ThrowingAreaManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ThrowingAreaManager : MonoBehaviour
+{
+    [SerializeField] private GameObject throwingAreaPrefab;
+    [SerializeField] private int throwingAreaCount = 4;
+    [SerializeField] private float spacing = 2f;
+    [SerializeField] private UnityEvent onAllThrowingAreaEntered = new UnityEvent();
+
+    private ThrowingArea[] throwingAreaControllers;
+
+    private void Start()
+    {
+        GenerateThrowingAreas();
+    }
+
+    private void GenerateThrowingAreas()
+    {
+        throwingAreaControllers = new ThrowingArea[throwingAreaCount];
+        for(int i = 0; i < throwingAreaCount; i++)
+        {
+            GameObject area = Instantiate(throwingAreaPrefab, transform);
+            area.transform.localPosition = new Vector3(i * spacing, 0, 0);
+            ThrowingArea controller = area.GetComponent<ThrowingArea>();
+            controller.Initialize(this);
+            throwingAreaControllers[i] = controller;
+        }
+    }
+
+    public void OnThrowingAreaEntered()
+    {
+        foreach (var controller in throwingAreaControllers)
+        {
+            if (!controller.IsEntered)
+            {
+                return;
+            }
+        }
+
+        onAllThrowingAreaEntered.Invoke();
+        Debug.Log("全プレイヤーが投擲エリアに到着しました");
+    }
+}
