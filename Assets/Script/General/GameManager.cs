63dfb29753d37519d5199ed40fa52827d3de5b47
SAI2338\scorp <scorpion.n13.ab@gmail.com>
2025-05-21 13:24:54 +0900

    2025/5/21/13:24 山本 Add:FrameSystem

--- Assets/Script/General/GameManager.cs
+++ Assets/Script/General/GameManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     [SerializeField] private float displayScoreTime = 3;
+    [SerializeField] int Num_NowFrame=1;
+    
 
     public PinManager pinManager;
     public ResetArea resetArea;
@@ -13,14 +17,43 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
     public GameObject score_object = null;
     public int totalScore;
 
+
+    private int[] NowFramePoint_0 = new int[11];
+    private int[] NowFramePoint_1 = new int[11];
+    private int[] NowFramePoint_2 = new int[11];
+    private int[] NowFramePoint_3 = new int[11];
+    int pp0,pp1,pp2,pp3;//a-dは仮置き,スコアを表す
+
+
     private void Start()
     {
     }
 
     private void Update()
     {
-        
+        if (false/*resetArea.OnTriggerEnter*/)
+        {
+            FrameSaveSystem();
+        }
     }
+
+    public void FrameSaveSystem()
+    {
+        
+        if (true) {// かつPlayer0であれば
+            pp0 = pinManager.GetKnockedDownPinCount();
+
+    NowFramePoint_0[Num_NowFrame] = pp0;//今回の得点
+        NowFramePoint_0[0] += NowFramePoint_0[Num_NowFrame];//合計点
+        }//if文の終わり
+
+
+
+        Num_NowFrame++;
+        resetArea.ResetGame();
+        Debug.Log("現在 +Num_NowFrame+ frameになりました");
+    }
+
 
     public void currentFrameResult()
     {
