957785097da55aed01a50659e6fb6955cbb82ceb
0yu-uy0 <e1c24059@oit.ac.jp>
2025-05-22 11:06:39 +0900

    2025/5/22 11:06 中井 add:120FPS

--- Assets/Script/General/GameManager.cs
+++ Assets/Script/General/GameManager.cs
@@ -27,7 +27,8 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
 
     private void Start()
     {
-
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 144;
     }
 
     private void Update()
