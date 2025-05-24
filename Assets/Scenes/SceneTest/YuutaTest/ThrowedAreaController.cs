b2700503a4af9176d62a5ec557237010ad66a84e
e1b24016 <e1b24016@oit.ac.jp>
2025-05-24 12:16:01 +0900

    2025/5/24 12:16 江端 Add:DashArea

--- Assets/Scenes/SceneTest/YuutaTest/ThrowedAreaController.cs
+++ Assets/Scenes/SceneTest/YuutaTest/ThrowedAreaController.cs
@@ -3,25 +3,12 @@ using UnityEngine;
 public class ThrowedAreaController : MonoBehaviour
 {
 
-
-    void Start()
-    {
-        
-    }
-
-
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             PlayerBase player = other.GetComponent<PlayerBase>();
             if (player) player.Throw();
-            else Debug.LogError("PlayerBaseがアタッチされていません!");
         }
     }
 
