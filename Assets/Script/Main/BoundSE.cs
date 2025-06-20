2a00206452d994632a8a40d775765113606135a9
0yu-uy0 <e1c24059@oit.ac.jp>
2025-06-21 03:20:24 +0900

    2025/06/21 3:20 中井 SE修正

--- Assets/Script/Main/BoundSE.cs
+++ Assets/Script/Main/BoundSE.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using System.Collections;
 
 public class BoundSE : MonoBehaviour
 {
+    private const string EFFECT_PATH = "Effect/Collision";
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("NPC"))
         {
             AudioManager.Instance.PlaySound(2);
+        }
+        if (collision.gameObject.CompareTag("Wall")){
+            AudioManager.Instance.PlaySound(3);
         }
     }
 }
