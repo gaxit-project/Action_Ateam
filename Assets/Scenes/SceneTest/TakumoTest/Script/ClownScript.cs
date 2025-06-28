5a8eb04d28cd004cfd9f74d8c323ca0a8b2f0be6
SAI2338\scorp <scorpion.n13.ab@gmail.com>
2025-06-28 14:09:39 +0900

    2025/6/25/14:10 山本 Add:ClownScriptの途中

---
+++ Assets/Scenes/SceneTest/TakumoTest/Script/ClownScript.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.TextCore;
+using static Player;
+
+public class ClownScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private GameObject Clown0;//Player0のClown
+    [SerializeField] private GameObject Clown1;//Player1のClown
+    [SerializeField] private GameObject Clown2;//Player2のClown
+    [SerializeField] private GameObject Clown3;//Player3のClown
+
+    private GameManager gameManager;
+    //Scoreの取得が必要なスクリプトを持ってくる
+
+    void Start()
+    {
+       gameManager = FindFirstObjectByType<GameManager>();
+        GameObject[] clowns = new GameObject[] { Clown0, Clown1, Clown2, Clown3 };
+
+
+        
+        int player = gameManager.NumHumanPlayers, bot = gameManager.NumBots;
+        int[] scores = new int[player + bot];
+        int i=0, j=0, isNum = 0;
+
+
+        
+        /*ScoreをPlayerーbotの順で順番に取得する*/    
+        while(i < player )
+        {
+            var playerdata = gameManager.GetPlayerScoreData("Player" + i+1);
+            scores[i] = playerdata.GetTotalScore();
+            i++;
+        }
+
+        while  (j < bot)
+        {
+            var botdata = gameManager.GetPlayerScoreData("Bot" + (isNum + 1));
+            scores[i] = botdata.GetTotalScore();/*botjのScore*/ ;
+            isNum++;
+            j++;
+        }
+
+
+
+
+
+
+        // 最大スコアのインデックスを取得
+        int maxIndex = 0;
+        for (i = 1; i <player+bot; i++)
+        {
+            
+            if (scores[i] > scores[maxIndex])
+            {
+                maxIndex = i;
+            }
+        }
+
+        // 該当するClownだけを有効化
+        clowns[maxIndex].SetActive(true);
+    }
+}
