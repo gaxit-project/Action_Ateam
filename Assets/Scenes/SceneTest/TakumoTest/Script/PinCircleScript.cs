adcf069be7d643c30a5ec8381a5d5673493e161a
SAI2338\scorp <scorpion.n13.ab@gmail.com>
2025-08-19 01:57:20 +0900

    2025/08/19 01:56 山本拓茂 add:上から見るやつでpinを自動取得できるようにした

--- Assets/Scenes/SceneTest/TakumoTest/Script/PinCircleScript.cs
+++ Assets/Scenes/SceneTest/TakumoTest/Script/PinCircleScript.cs
@@ -3,13 +3,27 @@ using UnityEngine.UI;
 
 public class PinCircleScript : MonoBehaviour
 {
-    /// <summary>
-    /// このscriptは対象Pinに対応するimgへ直接アタッチします。
-    /// そうすることによりPinが倒れるとimgが消えます。
-    /// </summary>
-    [SerializeField] private GameObject pinObject; // 対象のピン（Inspector で割り当て）
-
+    [SerializeField] private GameObject pinObject;
     private PinBase pinBase;
+
+    private void Reset()
+    {
+        // UIの名前から対応するピン名を取得
+        // 例: "pin1_img" → "pin1"
+        string targetName = gameObject.name.Replace("_img", "");
+
+        // シーン内から名前完全一致で検索
+        GameObject found = GameObject.Find(targetName);
+        if (found != null)
+        {
+            pinObject = found;
+        }
+    }
+
+    private void Awake()
+    {
+        Reset();
+    }
 
     void Start()
     {
