09a22e9ad76cf7fd12e1c9f3d1a102d4aacaee9f
SAI2338\scorp <scorpion.n13.ab@gmail.com>
2025-08-21 23:48:23 +0900

    2025/08/21 23:15 山本拓茂 Updata:ハイライトが倒れた時のピンの色になるように調整

--- Assets/Scenes/SceneTest/TakumoTest/Script/PinCircleScript.cs
+++ Assets/Scenes/SceneTest/TakumoTest/Script/PinCircleScript.cs
@@ -7,10 +7,12 @@ public class PinCircleScript : MonoBehaviour
     [SerializeField] private GameObject[] _pinPrefab = new GameObject[7];
     private PinBase pinBase;
 
+    // 体(0)か頭（１）のマテリアル色を取るか
+    private int targetMaterialIndex = 0;
+
     private void Reset()
     {
         // UIの名前から対応するピン名を取得
-        // 例: "pin1_img" → "pin1"
         string targetName = gameObject.name.Replace("_img", "");
 
         // シーン内から名前完全一致で検索
@@ -18,6 +20,21 @@ public class PinCircleScript : MonoBehaviour
         if (found != null)
         {
             pinObject = found;
+
+            // 名前に "Gold" が含まれていたら index = 1 に設定
+            if (pinObject.name.Contains("Gold"))
+            {
+                targetMaterialIndex = 1;
+            }
+            // 名前に "Black" が含まれていたら index = 1 に設定
+            else if (pinObject.name.Contains("Black"))
+            {
+                targetMaterialIndex = 1;
+            }
+            else
+            {
+                targetMaterialIndex = 0;
+            }
         }
     }
 
@@ -38,7 +55,12 @@ public class PinCircleScript : MonoBehaviour
     {
         if (pinBase != null && pinBase.GiveIsFallDown())
         {
-            GetComponent<Image>().enabled = false;
+            Renderer rend = pinObject.GetComponent<Renderer>();
+            if (rend != null && targetMaterialIndex < rend.materials.Length)
+            {
+                Color pinColor = rend.materials[targetMaterialIndex].color;
+                GetComponent<Image>().color = pinColor;
+            }
         }
     }
 }
